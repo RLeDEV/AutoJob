52cc7a19dc8ab69004277a191b82d5246e109167
רז לוי <bos901@gmail.com>
2020-08-01 18:00:51 +0300

    Added is_job_exist & add_new_job methods

--- Controller/Manage.py
+++ Controller/Manage.py
@@ -1,10 +1,15 @@
+import yaml
+
 from Controller.ChooseFactory import WebChooser
+from Driver.Mongo import Driver
+
 
 class Manage():
-    def __init__(self):
+    def __init__(self, enable_auto_send_resume=False):
         self._all_jobs = []
         self._websites = []
         self._num_of_jobs = 0
+        self.__auto_send_resume = enable_auto_send_resume
 
     def get_websites(self):
         return self._websites
@@ -22,6 +27,12 @@ class Manage():
 
     def add_website(self, url):
         try:
+            # Get MongoDB driver
+            with open("./Driver/config.yml", "r") as yml:
+                cfg = yaml.load(yml, Loader=yaml.FullLoader)
+            database = Driver().instance[cfg["mongodb"]["database"]]
+            collection = database[cfg["mongodb"]["collection"]]
+
             # Get the correct object by factory method
             type = self.get_type(url)
             website = WebChooser(type, url)
@@ -32,11 +43,32 @@ class Manage():
             self._websites.append(website)
             # Looping through all found jobs and check if already exist in the database
             for job in jobs:
-                if self.is_job_exist(job) is False:
-                    self._num_of_jobs += 1
-                    self._all_jobs.append(job)
+                if self.is_job_exist(job, collection) is False:
+                    self.add_new_job(job, collection)
         except Exception as e:
-            print('An error occured when tried to add a new website')
+            print('An error occurred when tried to add a new website')
 
-    def is_job_exist(self, job):
-        return False
+    def is_job_exist(self, job, collection):
+        try:
+            existence_status = collection.count_documents({'Job Title': job.get_description()}, limit=1)
+            if existence_status > 0:
+                # Job exist
+                return True
+            else:
+                # Job does not exist
+                return False
+        except Exception as e:
+            print('An error occurred when tried to find job existence status')
+            print(e)
+
+    def add_new_job(self, job, collection):
+        try:
+            jobDict = {"Job Title": job.get_description(),
+                       "Company": job.get_company(),
+                       "Link": job.get_link()}
+            collection.insert_one(jobDict)
+            job.print_job()
+            self._all_jobs.append(job)
+            self._num_of_jobs += 1
+        except Exception as e:
+            print("An error occurred when tried to add a new job to MongoDB Atlas")
