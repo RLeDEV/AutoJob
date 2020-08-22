7d1b3a46b744ca865d247dcbdeb8594db1cad126
רז לוי <bos901@gmail.com>
2020-08-22 14:54:56 +0300

    Fixed an error made by singleton instance

--- Driver/MailDriver.py
+++ Driver/MailDriver.py
@@ -1,34 +1,29 @@
 import smtplib
 import yaml
 
-
 class Mailer:
-
-    class __Mailer:
-        def __init__(self):
-            self.server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-
-    server = None
-
     def __init__(self):
         with open("./Driver/config.yml", "r") as yml:
             cfg = yaml.load(yml, Loader=yaml.FullLoader)
         self._gmail_user = cfg["mail"]["username"]
         self._gmail_password = cfg["mail"]["password"]
-        self._destination = cfg["mail"]["destination"]
-        if not self.server:
-            Mailer.server = Mailer.__Mailer().server
+        self._destination = "gmail@gmail.com"
+        self._server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 
-    def send_email(self, job, type, server):
+    def send_email(self, job, type):
         sent_from = self._gmail_user
         subject = "New job has been published from {}!".format(type)
-        body = "Job Description: " + job.get_description() + "\n Company: " + job.get_company() + "\n Link: " + job.get_link()
+        body = "Job Description: " + job.get_description() \
+               + "\n Company: " + job.get_company() \
+               + "\n Link: " + job.get_link()
         message = 'Subject: {}\n\n{}'.format(subject, body)
 
         try:
-            server.ehlo()
-            server.login(self._gmail_user, self._gmail_password)
-            server.sendmail(sent_from, self._destination, message)
+            self._server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            self._server.ehlo()
+            self._server.login(self._gmail_user, self._gmail_password)
+            self._server.sendmail(sent_from, self._destination, message)
+            self._server.quit()
             print('Email sent!')
         except Exception as e:
             print(e)
